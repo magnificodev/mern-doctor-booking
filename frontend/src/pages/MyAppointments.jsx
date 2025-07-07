4fb87c574c77ee4a745d0cd7518e18ada150fa4b
magnificodev <richardnguyen26th8@gmail.com>
2025-07-07 17:54:54 +0700

    create get user's appointments functionality

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,8 +1,55 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../contexts/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const MyAppointments = () => {
-    const { doctors } = useContext(AppContext);
+    const { backendUrl, token } = useContext(AppContext);
+    const [appointments, setAppointments] = useState([]);
+
+    const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+
+    const slotDateFormatter = (slotDate) => {
+        const dateArray = slotDate.split("-");
+        return `${dateArray[0]} ${months[dateArray[1] - 1]} ${dateArray[2]}`;
+    };
+
+    const getUserAppointments = async () => {
+        try {
+            const { data } = await axios.get(
+                `${backendUrl}/api/v1/user/appointments`,
+                {
+                    headers: {
+                        token,
+                    },
+                }
+            );
+            if (data.success) {
+                setAppointments(data.appointments.reverse());
+            }
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    };
+
+    useEffect(() => {
+        if (token) {
+            getUserAppointments();
+        }
+    }, [token]);
 
     return (
         <div>
@@ -10,44 +57,50 @@ const MyAppointments = () => {
                 My appointments
             </h4>
             <div>
-                {doctors.slice(0, 2).map((doctor) => (
+                {appointments.map((appointment) => (
                     <div
                         className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-4 border-b"
-                        key={doctor._id}
+                        key={appointment._id}
                     >
                         <div>
                             <img
                                 className="w-36 bg-indigo-50"
-                                src={doctor.image}
+                                src={appointment.docData.image}
                                 alt="Doctor Image"
                             />
                         </div>
                         <div className="flex-1 text-zinc-600 text-sm">
                             <h5 className="font-semibold text-base text-neutral-800">
-                                {doctor.name}
+                                {appointment.docData.name}
                             </h5>
                             <p className="text-sm">
-                                {doctor.speciality}
+                                {appointment.docData.speciality}
                             </p>
                             <h6 className="font-medium text-zinc-700 mt-1">
                                 Address:
                             </h6>
                             <p>
-                                {doctor.address.line1}
+                                {JSON.parse(appointment.docData.address).line1}
                                 <br />
-                                {doctor.address.line2}
+                                {JSON.parse(appointment.docData.address).line2}
                             </p>
                             <h6 className="font-medium text-zinc-700 mt-1">
                                 Date & Time:{" "}
                                 <span className="font-normal">
-                                    31 Apr 2025 | 04:30 PM
+                                    {`${slotDateFormatter(
+                                        appointment.slotDate
+                                    )} | ${appointment.slotTime}`}
                                 </span>
                             </h6>
                         </div>
                         <div></div>
                         <div className="flex flex-col justify-end gap-2 text-sm text-zinc-600">
-                            <button className="sm:min-w-48 border rounded px-8 py-2 hover:bg-primary hover:text-white transition-all duration-300">Pay Online</button>
-                            <button className="sm:min-w-48 border rounded px-8 py-2 hover:bg-red-600 hover:text-white transition-all duration-300">Cancel appointment</button>
+                            <button className="sm:min-w-48 border rounded px-8 py-2 hover:bg-primary hover:text-white transition-all duration-300">
+                                Pay Online
+                            </button>
+                            <button className="sm:min-w-48 border rounded px-8 py-2 hover:bg-red-600 hover:text-white transition-all duration-300">
+                                Cancel appointment
+                            </button>
                         </div>
                     </div>
                 ))}
